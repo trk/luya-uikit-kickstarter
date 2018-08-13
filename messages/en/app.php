8cbf369b30815572bd626499a87a0a9fd4e6db26
Iskender TOTOGLU <totoglu@gmail.com>
2018-08-13 20:44:14 +0300

    remove translations

--- messages/en/app.php
+++ messages/en/app.php
@@ -1,9 +1,3 @@
 <?php
 
-return [
-    'block.form.reservation' => 'Reservation Form',
-    'block.form.value.to' => ' to ',
-    'block.form.label.dates' => 'Arrival date / Departure date',
-    'block.form.placeholder.dates' => 'Please choose a date range',
-    'block.form.error.dates' => 'Please choose date range (arrival date / departure date)'
-];
+return [];
